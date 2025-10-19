eea247838b4e9e0afbec550608c37d77d70eca22
berlene <berlenebernabe12@gmail.com>
2025-10-19 10:25:22 +0800

    FOOTER TAILWIND

--- src/customs/Footer.js
+++ src/customs/Footer.js
@@ -3,29 +3,29 @@ import "../styles/customsStyle/Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-section">
-        <h4 className="footer-title">ABOUT US</h4>
-        <ul>
-          <li><a href="#who">Who we are</a></li>
-          <li><a href="#mission">Mission</a></li>
-          <li><a href="#vision">Vision</a></li>
+    <footer className="footer bg-[#001135] text-white flex justify-around py-10 px-15 flex-wrap mt-auto">
+      <div className="footer-section flex flex-col m-0 min-w-[200px] text-left">
+        <h4 className="footer-title font-bold text-base uppercase mb-5 text-white">ABOUT US</h4>
+        <ul className="list-none p-0 m-0">
+          <li className="my-2"><a href="#who" className="text-white no-underline text-sm font-normal hover:underline">Who we are</a></li>
+          <li className="my-2"><a href="#mission" className="text-white no-underline text-sm font-normal hover:underline">Mission</a></li>
+          <li className="my-2"><a href="#vision" className="text-white no-underline text-sm font-normal hover:underline">Vision</a></li>
         </ul>
       </div>
 
-      <div className="footer-section">
-        <h4 className="footer-title">CUSTOMER CARE</h4>
-        <ul>
-          <li><a href="#faqs">FAQs</a></li>
-          <li><a href="#feedback">Feedback & Inquiry</a></li>
+      <div className="footer-section flex flex-col m-0 min-w-[200px] text-left">
+        <h4 className="footer-title font-bold text-base uppercase mb-5 text-white">CUSTOMER CARE</h4>
+        <ul className="list-none p-0 m-0">
+          <li className="my-2"><a href="#faqs" className="text-white no-underline text-sm font-normal hover:underline">FAQs</a></li>
+          <li className="my-2"><a href="#feedback" className="text-white no-underline text-sm font-normal hover:underline">Feedback & Inquiry</a></li>
         </ul>
       </div>
 
-      <div className="footer-section">
-        <h4 className="footer-title">PARTNERSHIPS</h4>
-        <ul>
-          <li><a href="#nu-moa">NU MOA</a></li>
-          <li><a href="#playstation">PlayStation</a></li>
+      <div className="footer-section flex flex-col m-0 min-w-[200px] text-left">
+        <h4 className="footer-title font-bold text-base uppercase mb-5 text-white">PARTNERSHIPS</h4>
+        <ul className="list-none p-0 m-0">
+          <li className="my-2"><a href="#nu-moa" className="text-white no-underline text-sm font-normal hover:underline">NU MOA</a></li>
+          <li className="my-2"><a href="#playstation" className="text-white no-underline text-sm font-normal hover:underline">PlayStation</a></li>
         </ul>
       </div>
     </footer>
